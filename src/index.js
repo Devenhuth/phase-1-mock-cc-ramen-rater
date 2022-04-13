cf740507361a0f5f7ae9023999c7166c3f29becb
Deven <devenzachuth@gmail.com>
2022-04-13 10:59:14 -0500

    completed

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 // write your code here
+const menuImage = document.getElementById("ramen-detail")
+const ratingDisplay = document.getElementById('rating-display')
+const commentDisplay = document.getElementById('comment-display')
+const form = document.getElementById("new-ramen")
+const restName = document.getElementsByClassName("restaurant")
+const ramenName = document.getElementsByClassName("name")
+const ramenUrl = `http://localhost:3000/ramens`
+
+// form.addEventListener("submit", handleSubmit)
+
+fetch(`http://localhost:3000/ramens`)
+    .then(res => res.json())
+    .then(data => console.log(data));
+
